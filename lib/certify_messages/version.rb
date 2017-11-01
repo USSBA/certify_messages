54e383560079b37f6bbd3eee11136b1c3244cc25
Tyler Bolchoz <tybolt@gmail.com>
2017-11-01 10:33:28 -0400

    HUB-923 update version

--- lib/certify_messages/version.rb
+++ lib/certify_messages/version.rb
@@ -2,7 +2,7 @@ module CertifyMessages
   module VERSION
     MAJOR = 1
     MINOR = 1
-    PATCH = 0
+    PATCH = 1
     PRE_RELEASE = "".freeze # e.g., "-beta"
 
     STRING = ([MAJOR, MINOR, PATCH].join('.') + PRE_RELEASE).freeze
