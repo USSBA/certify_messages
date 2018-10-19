9dbd67b3c06da888b488dee42ae71f29774c817c
Doug Price <dprice@fearless.tech>
2018-10-19 18:47:01 -0400

    HUB-1650: API now allows archiving of conversations

--- lib/certify_messages/resources/conversation.rb
+++ lib/certify_messages/resources/conversation.rb
@@ -1,5 +1,6 @@
 module CertifyMessages
   # conversation class that handles getting and posting new conversations
+  # rubocop:disable Metrics/ClassLength
   class Conversation < Resource
     # base conversation finder
     # rubocop:disable Metrics/AbcSize
@@ -22,7 +23,7 @@ module CertifyMessages
       response = connection.request(method: :post,
                                     path: build_create_conversations_path,
                                     body: safe_params.to_json,
-                                    headers:  { "Content-Type" => "application/json" })
+                                    headers: { "Content-Type" => "application/json" })
       return_response(json(response.data[:body]), response.data[:status])
     rescue Excon::Error => error
       handle_excon_error(error)
@@ -50,6 +51,24 @@ module CertifyMessages
     rescue Excon::Error => error
       handle_excon_error(error)
     end
+
+    # rubocop:disable Metrics/MethodLength
+    def self.archive(params = nil)
+      return CertifyMessages.bad_request if empty_params(params)
+      return CertifyMessages.bad_request unless params.keys.include? :conversation_id
+      convo_id = params.delete :conversation_id
+      update_path = build_update_conversations_path convo_id
+      safe_params = archive_conversation_safe_params params
+      return CertifyMessages.unprocessable if safe_params.empty?
+      response = connection.request(method: :put,
+                                    path: update_path,
+                                    body: safe_params.to_json,
+                                    headers: { "Content-Type" => "application/json" })
+      return_response(json(response.data[:body]), response.data[:status])
+    rescue Excon::Error => error
+      handle_excon_error(error)
+    end
+    # rubocop:enable Metrics/MethodLength
     # rubocop:enable Metrics/AbcSize
 
     private_class_method
@@ -62,7 +81,14 @@ module CertifyMessages
     # helper for white listing parameters
     def self.conversation_safe_params(params)
       params = sanitize_params params
-      permitted_keys = %w[id subject application_id user_1 user_2 conversation_type]
+      permitted_keys = %w[id subject application_id user_1 user_2 conversation_type archived include_archived]
+      params.select { |key, _| permitted_keys.include? key.to_s }
+    end
+
+    # helper for white listing parameters
+    def self.archive_conversation_safe_params(params)
+      params = sanitize_params params
+      permitted_keys = %w[conversation_id archived]
       params.select { |key, _| permitted_keys.include? key.to_s }
     end
 
@@ -93,8 +119,13 @@ module CertifyMessages
       "#{path_prefix}/#{conversations_path}"
     end
 
+    def self.build_update_conversations_path(conversation_id)
+      "#{path_prefix}/#{conversations_path}/#{conversation_id}"
+    end
+
     def self.build_unread_message_counts_path(params)
       "#{path_prefix}/#{unread_message_counts_path}?#{URI.encode_www_form(params)}"
     end
   end
+  # rubocop:enable Metrics/ClassLength
 end
