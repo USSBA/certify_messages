cfb0bd891177983a93b639affee2aaeac67afdef
Joe Tseng <jtseng@fearsol.com>
2018-11-14 18:49:22 -0500

    HUB-1700: prepping params for testing v3.

--- spec/conversation_spec.rb
+++ spec/conversation_spec.rb
@@ -16,8 +16,17 @@ RSpec.describe CertifyMessages::Conversation, type: :feature do
       conversation_type: conversation_type
     }
   end
+  let(:params_v3) do
+    {
+        user1_uuid: "16028520-03c5-4bcc-9e90-d826613a4166",
+        user2_uuid: "fab8c5a8-e746-47d6-aef2-2f52c185317e",
+        application_uuid: "132ae1b3-734f-4313-a418-18667876fe56",
+        subject: convo_subject,
+        conversation_type: conversation_type
+    }
+  end
 
-  describe 'starting a new conversation', :vcr do
+  describe 'starting a new conversation in v1', :vcr do
     let(:conversation) do
       CertifyMessages::Conversation.create params
     end
@@ -27,7 +36,7 @@ RSpec.describe CertifyMessages::Conversation, type: :feature do
     end
   end
 
-  describe 'archiving a conversations', :vcr do
+  describe 'archiving a conversation in v1', vcr: false do
     let(:conversation) { CertifyMessages::Conversation.create params }
     let(:archived_convo) do
       CertifyMessages::Conversation.archive conversation_id: conversation[:body]['id'], archived: true
